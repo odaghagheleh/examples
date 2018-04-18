7de414ee7fd0c0cd7f53c9f65353415f2d43490c
Own Daghagheleh <>
2018-04-18 15:11:00 +1000

    New Styling

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -13,7 +13,7 @@ export default class MainForm extends Component {
     render() {
         const steps =
             [
-                { name: 'Step 1', component: <DisputeCreditTransaction history={this.props.history}/> },
+                { name: 'Step 1', component: <DisputeCreditTransaction history={this.props.history} /> },
                 { name: 'Step 2', component: <DecisionQuestionApp /> },
                 { name: 'Step 3', component: <CreditCardQuestionnaire /> },
                 { name: 'Step 4', component: <AdditionalQuestionnaire /> },
@@ -24,20 +24,41 @@ export default class MainForm extends Component {
 
         let wrapperClass = "gray-bg " + this.props.location.pathname;
         return (
-
             <div id="wrapper">
                 <div id="page-wrapper" className={wrapperClass}>
-                    <div className='example' >
-                        <div className='step-progress'>
-                            <StepZilla
-                                steps={steps}
-                                preventEnterSubmission={true}
-                                nextTextOnFinalActionStep={"Save"}
-                                nextButtonCls= "btn btn-prev btn-primary btn-sm pull-right"
-                                backButtonCls= "btn btn-next btn-primary btn-sm pull-left"
-                                nextTextOnFinalActionStep= "Submit"
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="ibox float-e-margins">
+                                    <div class="ibox-title">
+                                        <h2>Dispute Credit Transaction</h2>
+                                        <div class="ibox-tools">
+                                            <a class="collapse-link">
+                                                <i class="fa fa-chevron-up"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="ibox-content">
 
-                            />
+                                        <div className='example' class="row">
+                                            <div class="col-lg-12">
+                                                <div className='step-progress'>
+                                                    <StepZilla
+                                                        steps={steps}
+                                                        preventEnterSubmission={true}
+                                                        nextTextOnFinalActionStep={"Save"}
+                                                        nextButtonCls="btn btn-prev btn-primary btn-sm pull-right"
+                                                        backButtonCls="btn btn-next btn-primary btn-sm pull-left"
+                                                        nextTextOnFinalActionStep="Submit"
+
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
